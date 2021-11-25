393743e722ed4f91b4583c83fe9d1b49b2f4be60
Mason <Mason.Worthen@msj.edu>
2021-11-25 09:55:15 -0500

    November 23, 2021 healthyheartapi v 1.0.0

--- controllers/api.controller.js
+++ controllers/api.controller.js
@@ -3,33 +3,31 @@ const firebase = require("../config/firebase.config");
 const developerKeyGenerator = require("random-key");
 
 function addDeveloper(req,res){
-    const uid = {
-        name:"Mason Worthen",
-        email:"Mason.Worthen@msj.edu"
-    }
-    firebase.admin.auth()
-        .createCustomToken(JSON.stringify((uid)))
-        .then((customToken) => {
-            res.send(customToken);
-        })
-        .catch((error) => {
-            console.log('Error creating custom token:', error);
+        let developerKey = developerKeyGenerator.generate()
+        let name  = req.body.name;
+        let email = req.body.email;
+        const db = firebase.getDatabase(firebase.firebaseApp);
+        firebase.set(firebase.ref(db, 'users/'+developerKey), {
+            email: email,
+            name:name,
         });
+        res.send({key:developerKey})
 }
 
 function authenticateDeveloper(req,res,next){
-    let token = req.query.token
-    firebase.signInWithCustomToken(firebase.getAuth(),token)
-        .then((userCredential) => {
-            let user = userCredential.user;
+    const db = firebase.getDatabase(firebase.firebaseApp)
+    const referenceCount = firebase.ref(db);
+    firebase.get(firebase.child(referenceCount, '/users/'+req.query.token)).then((snapshot) => {
+        if (snapshot.exists()) {
+            let data = snapshot.val();
+            return next();
+        } else {
+            res.redirect('/');
+        }
+    }).catch((error) => {
 
-        })
-        .catch((error) => {
-            var errorCode = error.code;
-            var errorMessage = error.message;
-
-            // ...
-        });
+        res.redirect('/');
+    });
 
 }
 
